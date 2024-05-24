c0a0f0c8a78e6b5dac5de36c903a743d9002ef52
bunyod <naimov.bunyod2021@gmail.com>
2024-05-24 14:40:12 +0500

    start

---
+++ modul_2/lesson_3.py
@@ -0,0 +1,19 @@
+# names = ["Abdulla", "Baxodir", "Ulug'bek", "Alisher"]
+# for name in names:
+#     print(f"Salom {name} yaxshimisiz?")
+
+
+# cities = ["Tashkent", "Abu-Dabiy", "Istambul", "Moscow", "New-York"]
+#
+# for city in cities:
+#     print(f"{city} juda ham chiroyli shahar!")
+
+
+# name = "Shohruh"
+# for i in name:
+#     print(i.upper())
+
+
+
+for i in range(5, 50 ,5):
+    print(i)
