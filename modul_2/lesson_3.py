9b6fdcc6c51c18bd708cab29f6ac138346c765a0
bunyod <naimov.bunyod2021@gmail.com>
2024-05-24 16:52:33 +0500

    start

--- modul_2/lesson_3.py
+++ modul_2/lesson_3.py
@@ -13,7 +13,6 @@
 # for i in name:
 #     print(i.upper())
 
-
-
-for i in range(5, 50 ,5):
-    print(i)
+for i in range(1, 20):
+    if i % 3 == 0:
+        print(i)
---
+++ modul_2/lesson_4.py
@@ -0,0 +1,2 @@
+for number in range(1, 11):
+    print(f"{2} x {number} = {2 * number}")
